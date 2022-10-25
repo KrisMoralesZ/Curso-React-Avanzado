f900b2e468dca7f653cc3de3cf3a6e525e14efad
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2022-10-25 14:53:08 -0700

    first hook

--- src/components/ListOfCategory/index.js
+++ src/components/ListOfCategory/index.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Category } from "../Category";
 import { List, Item } from "./styles";
-import { categories } from "../../../api/db.json"
 
 export const ListOfCategories = () => {
+
+  cons[categories, setCategories] = useState([]);
+
+  useEffect(function () {
+    fetch('https://petgram-server.midudev.now.sh/categories')
+      .then(res => res.json())
+      .then(response => {
+        setCategories(response)
+      })
+  }, [])
+
   return (
     <List>
       {
--- src/components/ListOfPhotoCard/index.js
+++ src/components/ListOfPhotoCard/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { PhotoCard } from "../PhotoCard"
 
-export const ListOfPhotoCards = () => {
+const ListOfPhotoCards = () => {
   return (
     <ul>
       {[1, 2, 3].map(id => <PhotoCard key={id} />)}
     </ul>
   )
 }
+export default ListOfPhotoCards;
